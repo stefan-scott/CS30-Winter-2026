e268f8efe78301873d20f74c88c54c1c843e3d8d
stefan-scott <scottst@spsd.sk.ca>
2026-02-23 11:52:28 -0600

    demo complete

--- 04 State Variables and Real Time/sketch.js
+++ 04 State Variables and Real Time/sketch.js
@@ -4,7 +4,7 @@
 
 // Global Variables
 let shapeState = 0; //0- Circle    1- Square
-                    //2- Triangle  3- Starbust
+//2- Triangle  3- Starbust
 let startTime;
 
 
@@ -24,25 +24,34 @@ function setup() {
 function draw() {
   background(220);
   drawShape();
-
+  manageTime();
 }
 
-function manageTime(){
+function manageTime() {
   // this function will reset our timer
   // (and update shapeState) once every
-  // 
+  // 1000 milliseconds.
+  let elapsedTime = millis() - startTime;
+  if (elapsedTime > 1000) {
+    updateState();
+    startTime = millis();
+  }
 }
 
-function keyPressed(){
+function keyPressed() {
+  updateState();
+}
+
+function updateState() {
   shapeState++;
-  if(shapeState > 3) shapeState = 0;
+  if (shapeState > 3) shapeState = 0;
 }
 
-function drawShape(){
+function drawShape() {
   // function inspects the shapeState variable
   // and draws the appropriate shape on the canvas
-  let x = width/2;    let y = height/2;
-  switch(shapeState){
+  let x = width / 2; let y = height / 2;
+  switch (shapeState) {
     case 0:
       circle(x, y, 150);
       break;
@@ -50,13 +59,13 @@ function drawShape(){
       square(x, y, 150);
       break;
     case 2:
-      triangle(x-50, y+50, x+50, y+50, x, y-25);
+      triangle(x - 50, y + 50, x + 50, y + 50, x, y - 25);
       break;
     case 3:
-      for(let i = 0; i < 30; i++){
-        let x2 = random(x-80, x+80);
-        let y2 = random(y-80, y+80);
-        line(x,y,x2,y2);
+      for (let i = 0; i < 30; i++) {
+        let x2 = random(x - 80, x + 80);
+        let y2 = random(y - 80, y + 80);
+        line(x, y, x2, y2);
       }
       break;
   }
