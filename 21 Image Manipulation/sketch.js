78b48e4b977f9b87a35d6f2ee03f57b3757aac76
stefan-scott <scottst@spsd.sk.ca>
2026-04-23 12:55:18 -0600

    demo done

--- 21 Image Manipulation/sketch.js
+++ 21 Image Manipulation/sketch.js
@@ -7,27 +7,77 @@
 // Part 2:  using video
 
 let myImage;
+let myVideo;
 
 function preload(){
   //called BEFORE setup. Won't conclude
   //until all loads are complete.
-   myImage = loadImage("assets/aviator.png");
+  myImage = loadImage("assets/aviator.png");
 }
 
 function setup() {
-  createCanvas(myImage.width, myImage.height);
+  //createCanvas(myImage.width, myImage.height);
+  createCanvas(640,480);
+  myVideo = createCapture(VIDEO);
   pixelDensity(1); 
+  //myVideo.hide();
 }
 
 function draw() {
   background(220);
-  image(myImage, 0,0);
-
+  //image(myImage, 0,0);   //STILL IMAGE
+  image(myVideo,0,0);
   //access and modify the pixels on the Canvas
   loadPixels();  //dumps data from canvas into array
-  //boost();
-  greyscale();
-  updatePixels();
+  background(0);
+  
+  // boost();
+  // greyscale();
+  // updatePixels();
+  // textImage();
+  randomSpots();
+}
+
+function randomSpots(){
+  randomSeed(4);
+  let size = 5;
+  noStroke();
+  for (let i = 0 ; i < 15000; i++){
+    let x = int(random(0,width));
+    let y = int(random(0,height));
+    if(dist(x,y,mouseX, mouseY)< 150){
+      let c = getColor(x,y);
+    fill(c);
+    circle(x,y,size);
+    }
+    
+  }
+}
+
+function getColor(x,y){
+  //return color located at an x,y coordinate
+  let index = ((y*width) + x)*4;
+  let r = pixels[index];
+  let g = pixels[index+1];
+  let b = pixels[index+2];
+  return color(r,g,b);
+}
+
+function textImage(){
+  fill(255);
+  let scaleAmount = 10;
+  textSize(scaleAmount);
+
+  for(let x = 0; x < width; x+= scaleAmount){
+    for(let y = 0; y < height; y += scaleAmount){
+      let avg = getAvg(x,y);  //0-255
+      if (avg > 210) text("😭", x, y);
+      else if(avg > 170) text("T", x, y);
+      else if(avg > 130) text("x", x, y);
+      else if(avg > 90) text(":", x, y);
+      else if(avg > 45) text(".", x, y);
+    }
+  }
 }
 
 function boost(){
@@ -50,6 +100,7 @@ function getAvg(x,y){
   let b = pixels[index+2];
   return (r+g+b) / 3
 }
+
 function greyscale() {
   // use the average intensity of each pixel
   // to represent it as a shade of grey.
@@ -60,6 +111,7 @@ function greyscale() {
     }
   }
 }
+
 function setPixel(x,y,r,g,b){
   //x,y → pixel location
   //r,g,b → color values
